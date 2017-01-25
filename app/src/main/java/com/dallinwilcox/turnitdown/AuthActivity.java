d5e44b1ed7d3e06dd2967686a2a446e37f539eea
dallinwilcox <dcwilcox@gmail.com>
2017-01-24 21:36:19 -0600

    Added AuthActivity

---
+++ app/src/main/java/com/dallinwilcox/turnitdown/AuthActivity.java
@@ -0,0 +1,87 @@
+package com.dallinwilcox.turnitdown;
+
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.ResultCodes;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+
+/**
+ * Referenced:
+ * https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md
+ */
+public class AuthActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 42;
+    View view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+        view = findViewById(R.id.base_view);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (null != auth.getCurrentUser()) {
+            // already signed in
+        } else {
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                    new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                            .setIsSmartLockEnabled(!BuildConfig.DEBUG)
+                            .build(),
+                    RC_SIGN_IN);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == ResultCodes.OK) {
+                startActivity(DeviceListActivity.createIntent(this, response));
+                finish();
+                return;
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    showSnackbar(R.string.sign_in_cancelled);
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackbar(R.string.no_internet_connection);
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackbar(R.string.unknown_error);
+                    return;
+                }
+            }
+
+            showSnackbar(R.string.unknown_sign_in_response);
+        }
+    }
+
+    private void showSnackbar(int stringId) {
+        Snackbar.make(view, stringId, Snackbar.LENGTH_LONG);
+    }
+}
--- app/src/main/java/com/dallinwilcox/turnitdown/DeviceListActivity.java
+++ app/src/main/java/com/dallinwilcox/turnitdown/DeviceListActivity.java
@@ -38,8 +38,7 @@ import java.util.List;
  */
 
 public class DeviceListActivity extends AppCompatActivity {
-
-    private static final int RC_SIGN_IN = 42;
+    public static final String IDP_RESPONSE = "IdpResponse";
     /**
      * Whether or not the activity is in two-pane mode, i.e. running on a tablet
      * device.
@@ -78,56 +77,15 @@ public class DeviceListActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        if (null != auth.getCurrentUser()) {
-            // already signed in
-        }
-        else {
-            startActivityForResult(
-                    AuthUI.getInstance()
-                            .createSignInIntentBuilder()
-                            .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
-                                    new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
-                            .setIsSmartLockEnabled(!BuildConfig.DEBUG)
-                            .build(),
-                    RC_SIGN_IN);
-        }
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
-        if (requestCode == RC_SIGN_IN) {
-            IdpResponse response = IdpResponse.fromResultIntent(data);
-
-            // Successfully signed in
-            if (resultCode == ResultCodes.OK) {
-                startActivity(SignedInActivity.createIntent(this, response));
-                finish();
-                return;
-            } else {
-                // Sign in failed
-                if (response == null) {
-                    // User pressed back button
-                    showSnackbar(R.string.sign_in_cancelled);
-                    return;
-                }
-
-                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
-                    showSnackbar(R.string.no_internet_connection);
-                    return;
-                }
-
-                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
-                    showSnackbar(R.string.unknown_error);
-                    return;
-                }
+        protected void onStart() {
+            super.onStart();
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+            if (null != auth.getCurrentUser()) {
+                // already signed in
             }
-
-            showSnackbar(R.string.unknown_sign_in_response);
-        }
+            else {
+                startActivity( new Intent (getApplicationContext(), AuthActivity.class));
+            }
     }
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
@@ -201,5 +159,9 @@ public class DeviceListActivity extends AppCompatActivity {
                 return super.toString() + " '" + mContentView.getText() + "'";
             }
         }
+    }
+    public static Intent createIntent (Context context, IdpResponse response)
+    {
+        return new Intent (context, DeviceListActivity.class).putExtra(IDP_RESPONSE, response);
     }
 }
