33bbc409e4590cbc8250354e1674ab2d1a9d91f3
dallinwilcox <dcwilcox@gmail.com>
2017-02-25 16:12:42 -0600

    beginning to implement FireBase database

--- app/src/main/java/com/dallinwilcox/turnitdown/DeviceListAdapter.java
+++ app/src/main/java/com/dallinwilcox/turnitdown/DeviceListAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,19 +12,50 @@ import android.widget.TextView;
 
 import com.dallinwilcox.turnitdown.data.Device;
 import com.dallinwilcox.turnitdown.inf.OnItemClick;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 /**
  * Created by dcwilcox on 2/4/2017.
+ * referenced:
+ *  https://firebase.google.com/docs/database/android/start/
+ *  https://howtofirebase.com/firebase-data-modeling-939585ade7f4
  */
 
 public class DeviceListAdapter extends RecyclerView.Adapter<DeviceListAdapter.DeviceListViewHolder> {
+    public static final String TAG = "DeviceListAdapter";
     private ArrayList<Device> adapterDeviceList;
     private OnItemClick itemClick;
 
+
     public DeviceListAdapter() {
+        //Init DB
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        //example DB Write
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello, World!");
         adapterDeviceList = new ArrayList<>();
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 
     @Override
