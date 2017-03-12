04044d9d1df40c5a9c3a594deb41f717a5087f17
dallinwilcox <dcwilcox@gmail.com>
2017-03-11 22:59:43 -0600

    added functions references

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
 var functions = require('firebase-functions');
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
+//referenced
+// https://firebase.googleblog.com/2016/08/sending-notifications-between-android.html
+//https://github.com/firebase/functions-samples/blob/master/fcm-notifications/functions/index.js
+// https://firebase.google.com/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
